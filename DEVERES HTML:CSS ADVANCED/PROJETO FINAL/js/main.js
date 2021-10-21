3f3c5e73c310bce6ab5e4677c54f2cf21d56fcbc
Caio Araujo <caiodaluz2004@gmail.com>
2021-10-21 20:29:27 -0300

    Atualização Projeto

--- DEVERES HTML:CSS ADVANCED/PROJETO FINAL/js/main.js
+++ DEVERES HTML:CSS ADVANCED/PROJETO FINAL/js/main.js
@@ -1,3 +1,17 @@
+var bt_reservar =  document.getElementById("reservar");
+var bt_login = document.getElementById("bt-login");
+var bt_ver_reservas =  document.getElementById("ver-reservas");
+var bt_logout = document.getElementById("bt-logout");
+var msg_exito = document.getElementById("msg-exito");
+var msg_erro = document.getElementById("msg-erro");
+
+if (sessionStorage.getItem("logado") != null) {
+    bt_reservar.style.display = "none";
+    bt_login.style.display = "none";
+    bt_ver_reservas.style.display = "block";
+    bt_logout.style.display = "block";
+}
+
 class Funcionario{
     login;
     senha;
@@ -14,17 +28,19 @@ class Funcionario{
 
         if (this.login === "func000" && this.senha === "func123") {
 
-            document.getElementById("msg-exito").style.display = "block";
+            msg_exito.style.display = "block";
+
+            sessionStorage.setItem("logado", "logado");
 
             setTimeout(() => {
 
                 document.getElementsByClassName("btn-close")[0].click();
-                document.getElementById("msg-exito").style.display = "none";
+                msg_exito.style.display = "none";
 
-                document.getElementById("reservar").style.display = "none";
-                document.getElementById("bt-login").style.display = "none";
-                document.getElementById("ver-reservas").style.display = "block";
-                document.getElementById("bt-logout").style.display = "block";
+                bt_reservar.style.display = "none";
+                bt_login.style.display = "none";
+                bt_ver_reservas.style.display = "block";
+                bt_logout.style.display = "block";
 
                 document.getElementById("input-login").value = "";
                 document.getElementById("input-senha").value = "";
@@ -33,13 +49,13 @@ class Funcionario{
         
         } else{
 
-            document.getElementById("msg-erro").style.display = "block";
+            msg_erro.style.display = "block";
             document.getElementById("input-login").value = "";
             document.getElementById("input-senha").value = "";
 
             setTimeout(() => {
 
-                document.getElementById("msg-erro").style.display = "none";
+                msg_erro.style.display = "none";
 
             }, 5000)
         }
@@ -58,5 +74,33 @@ function login() {
 }
 
 function logout() {
-    location.reload();
+    sessionStorage.removeItem("logado");
+    bt_reservar.style.display = "block";
+    bt_login.style.display = "block";
+    bt_ver_reservas.style.display = "none";
+    bt_logout.style.display = "none";
+}
+
+class Cliente{
+    nome;
+    email;
+
+    constructor(nome, email){
+        this.nome = nome;
+        this.email = email;
+    }
+}
+
+class Reserva{
+    cliente = new Cliente();
+    dt_chegada;
+    dt_saida;
+    qntd_quartos;
+
+    constructor(cliente, dt_chegada, dt_saida, qntd_quartos){
+        this.cliente = cliente;
+        this.dt_chegada = dt_chegada;
+        this.dt_saida = dt_saida;
+        this.dt_saida = qntd_quartos;
+    }
 }
