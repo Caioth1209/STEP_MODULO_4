561f7580f59fa360b688e261ae73af5f34d8ba85
Caio Araujo <caiodaluz2004@gmail.com>
2021-10-23 17:21:28 -0300

    Atualização projeto

--- DEVERES HTML:CSS ADVANCED/PROJETO FINAL/js/main.js
+++ DEVERES HTML:CSS ADVANCED/PROJETO FINAL/js/main.js
@@ -68,7 +68,7 @@ function login() {
     let login = document.getElementById("input-login").value;
     let senha = document.getElementById("input-senha").value;
 
-    var f1 = new Funcionario(login,senha);
+    var f1 = new Funcionario(login, senha);
     
     f1.verificaLogin();
 }
@@ -80,6 +80,147 @@ function logout() {
     bt_ver_reservas.style.display = "none";
     bt_logout.style.display = "none";
 }
+
+window.onload = function(){ 
+    
+    document.getElementById("input-nome").value = "";
+    document.getElementById("input-email").value = "";
+    document.getElementById("input-dt-chegada").value = "";
+    document.getElementById("input-dt-saida").value = "";
+    document.getElementById("btn-preReserva").disabled = true;
+}
+
+let validNome = 1;
+
+function validaNome(c){
+
+    var msg = document.getElementById("msg-erro-nome");
+	
+    var nome = c.value.replace(" ", "");
+
+    var regex = /[0-9]/;
+    
+    if(regex.test(nome)){
+    
+        c.value = "";
+
+        msg.style.display = "block";
+
+        validNome = 1;
+        document.getElementById("btn-preReserva").disabled = true;
+                
+    } else {
+    
+        if (c.value.trim().length < 3) {
+        
+            c.value = "";
+    
+            msg.style.display = "block";	
+        
+            validNome = 1;
+
+            document.getElementById("btn-preReserva").disabled = true;
+                        
+        } else {
+            msg.style.display = "none";
+            validNome = 0;
+            if ((validNome + validEmail + validDts + validDtc)==0) {
+                document.getElementById("btn-preReserva").disabled = false;
+            }
+        }
+
+    }
+        
+}
+
+let validEmail = 1;
+
+function validaEmail(c){
+
+    var msg = document.getElementById("msg-erro-email");
+	
+    var regex = /\S+@\S+\.\S+/;
+
+    if (regex.test(c.value)) {
+        msg.style.display = "none";
+        validEmail = 0;
+        if ((validNome + validEmail + validDts + validDtc)==0) {
+            document.getElementById("btn-preReserva").disabled = false;
+        }
+    } else {
+        c.value = "";
+        msg.style.display = "block";
+        validEmail = 1;
+        document.getElementById("btn-preReserva").disabled = true;
+    }
+        
+}
+
+var mes = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+
+let validDtc = 1;
+
+function validaDtChegada(c){
+    var dtSaida = document.getElementById("dt-saida");
+    var input_dtSaida = document.getElementById("input-dt-saida");
+    var msg = document.getElementById("msg-erro-dtc");
+
+    var data = new Date();
+
+    var dtAtual = data.getFullYear() + "-" + mes[data.getMonth()] + "-" + data.getDate();
+
+    if (c.value >= dtAtual) {
+        msg.style.display = "none";
+        validDtc = 0;
+        dtSaida.style.display = "block";
+
+        if (input_dtSaida.value < c.value) {
+            input_dtSaida.value = "";
+            validDts = 1;
+            document.getElementById("btn-preReserva").disabled = true;
+        }
+
+    } else {
+        c.value = "";
+        msg.style.display = "block";
+        dtSaida.style.display = "none";
+        validDtc = 1;
+        document.getElementById("btn-preReserva").disabled = true;
+    }   
+}
+
+let validDts = 1;
+
+function validaDtSaida(c){
+
+    var msg = document.getElementById("msg-erro-dts");
+
+    var dtc = document.getElementById("input-dt-chegada");
+
+    var data = new Date();
+
+    if (c.value > dtc.value) {
+        msg.style.display = "none";
+        validDts = 0;
+        if ((validNome + validEmail + validDts + validDtc)==0) {
+            document.getElementById("btn-preReserva").disabled = false;
+        }
+    } else {
+        c.value = "";
+        msg.style.display = "block";
+        validDts = 1;
+        document.getElementById("btn-preReserva").disabled = true;
+    }   
+}
+
+function validaQuartos(c) {
+    
+    if (isNaN(c.value) || c.value < 1 || c.value > 10) {
+        c.value = 1;
+    }
+
+}
+
 
 class Cliente{
     nome;
@@ -102,5 +243,10 @@ class Reserva{
         this.dt_chegada = dt_chegada;
         this.dt_saida = dt_saida;
         this.dt_saida = qntd_quartos;
+    }
+
+    verificaReserva(){
+        
+
     }
 }
